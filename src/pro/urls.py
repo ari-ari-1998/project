c935e53c31ee0afb168339cec39f8ed1032df77c
ari-ari-1998 <ryotaro2411@gmail.com>
2023-09-10 04:11:45 +0900

    削除昨日まで

--- src/pro/urls.py
+++ src/pro/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListView, DetailView , CreateView
+from .views import ListView, DetailView , CreateView, UpdateFormView, DeleteView
 
 urlpatterns = [
     path("", ListView, name="list"),
-    path("detail/<int:number>/", DetailView, name="detail"),
+    path("detail/<int:pk>/", DetailView, name="detail"),
     path("create/", CreateView, name="create"),
+    path("update/<int:pk>/", UpdateFormView, name="update"),
+    path("delete/<int:pk>/", DeleteView, name="delete"),
 ]
--- src/pro/views.py
+++ src/pro/views.py
@@ -1,27 +1,64 @@
-from django.shortcuts import render
-from random import randint
+from django.shortcuts import render, get_object_or_404
+from .models import NippoModel
+from .forms import  NippoFormClass
+from django.shortcuts import redirect
+
 
 def ListView(request):
-	return render(request, "pro/list.html")
-
-
-
-def DetailView(request):
-    template_name="pro/detail.html"
-    random_int = randint(1,10)
-    ctx = {
-        "random_number": random_int,
-    }
+    template_name = "pro/list.html"
+    ctx = {}
+    qs = NippoModel.objects.all()
+    ctx["object_list"] = qs
     return render(request, template_name, ctx)
+
+
+
+def DetailView(request, pk):
+     template_name = "pro/detail.html"
+     ctx = {}
+     #q = NippoModel.objects.get(pk=pk)
+     q = get_object_or_404(NippoModel, pk=pk)
+     ctx["object"] = q
+     return render(request, template_name, ctx)
 
 def CreateView(request):
     template_name="pro/form.html"
+    form = NippoFormClass(request.POST or None)
+    ctx = {"form": form}
+    if form.is_valid():
+        company = form.cleaned_data["company"]
+        company_id = form.cleaned_data["company_id"]
+        company_password = form.cleaned_data["company_password"]
+        obj = NippoModel(company=company, company_id=company_id, company_password=company_password)
+        obj.save()
+        return redirect("list")
+    return render(request, template_name, ctx)
 
+def UpdateFormView(request, pk):
+    template_name = "pro/form.html"
+    #obj = NippoModel.objects.get(pk=pk)
+    obj = get_object_or_404(NippoModel, pk=pk)
+    initial_values = {"company": obj.company, "company_id":obj.company_id, "company_password":obj.company_password}
+    form = NippoFormClass(request.POST or initial_values)
+    ctx = {"form": form}
+    ctx["object"] = obj
+    if form.is_valid():
+        company = form.cleaned_data["company"]
+        company_id = form.cleaned_data["company_id"]
+        company_password = form.cleaned_data["company_password"]
+        obj.company = company
+        obj.company_id = company_id
+        obj.company_password = company_password
+        obj.save()
+        if request.POST:
+            return redirect("list")
+    return render(request, template_name, ctx)
+
+def DeleteView(request, pk):
+    template_name = "pro/delete.html"
+    obj = get_object_or_404(NippoModel, pk=pk)
+    ctx = {"object": obj}
     if request.POST:
-        company = request.POST["company"]
-        company_id = request.POST["company_id"]
-        company_password = request.POST["company_passowrd"]
-
-    #受け取った値で必要な処理を行います
-        
-    return render(request, template_name)
+        obj.delete()
+        return redirect("list")
+    return render(request, template_name, ctx)
